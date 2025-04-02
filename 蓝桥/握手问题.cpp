6e86d5897b838aec4189a7c390e8e0103f7942fd
hjhhoni <142111479+hjhhoni@users.noreply.github.com>
2025-04-02 15:05:15 +0800

    1

--- 蓝桥/握手问题.cpp
+++ 蓝桥/握手问题.cpp
@@ -1,15 +1,9 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-typedef long long ll;
-const int N = 1e9+7;
-
-void solve(){
-
-}
-
-int main() {
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    int t=1;//cin>>t;
-    while(t--) solve();
+int main(){
+    int sum=0;
+    for(int i=7;i<=49;i++) sum+=i;
+    cout<<sum;
     return 0;
 }
+//对于第一个人来说 除了自己以外要跟其他49人握手 所以第一个是49 //对于第二个人来说 第一个人主动跟我握手了 有一次不算 所以第二个是48.。。 //以此类推 第43个人就是7 到了最后七个人呢 因为互相都没有握手 并且7个人都被前面的人握过手了 所以都是0
